3164904d41260a2c548d04770f067e57ebafe8c8
xulame <alkan.muhammet.47@gmail.com>
2023-08-06 16:22:03 +0300

    Email Service is configured

--- src/main/java/com/empatik/backend/email/EmailService.java
+++ src/main/java/com/empatik/backend/email/EmailService.java
@@ -1,7 +1,5 @@
 package com.empatik.backend.email;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,20 +14,25 @@ public class EmailService implements EmailSender{
 
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
-    //private final JavaMailSender mailSender; -> Mail sender eklenecek
+
+    private  final JavaMailSender mailSender;
+
+    /*public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }*/
     @Override
     @Async
     public void send(String to, String email) {
-        /*try{
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try{
+            jakarta.mail.internet.MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,"utf-8");
             helper.setText(email, true);
             helper.setTo(to);
             helper.setSubject("Confirm your email");
             helper.setFrom("alkan.muhammet.47@gmail.com");
-        }catch (MessagingException e){
-            LOGGER.error("failed to send email",e);
-            throw new IllegalStateException("failed to send email");
-        }*/
+        } catch (jakarta.mail.MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
